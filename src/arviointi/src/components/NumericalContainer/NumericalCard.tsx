a86d813e9d7a5a347af414fa54e4b434fb3f3ce2
Jani Sourander <jani.sourander@kamk.fi>
2025-01-21 08:10:30 +0200

    Refactor NumericalCard to modular structure

--- src/arviointi/src/components/NumericalContainer/NumericalCard.tsx
+++ src/arviointi/src/components/NumericalContainer/NumericalCard.tsx
@@ -1,4 +1,4 @@
-import { OatGradeType } from "../../types/oat-grade-types";
+import { NumericalCriteria, OatGradeType, GradeOption } from "../../types/oat-grade-types";
 import { GradeStateContainer } from "../../types/gradestates";
 import "./NumericalCard.css";
 
@@ -7,59 +7,89 @@ interface INumericalCard {
   handleGradeChange: (gradeinfo: GradeStateContainer) => void;
   selectedGrades: GradeStateContainer;
 }
+const numericalRadioButtons = ({
+  criterion,
+  gradeOption,
+  selectedGrades,
+  handleGradeChange,
+}: {
+  criterion: NumericalCriteria;
+  gradeOption: GradeOption;
+  selectedGrades: GradeStateContainer;
+  handleGradeChange: (gradeinfo: GradeStateContainer) => void;
+}) => (
+  <div className="num-criterion-radio" key={gradeOption.grade}>
+    <input
+      type="radio"
+      id={`${criterion.id}-${gradeOption.grade}`}
+      name={criterion.id}
+      value={gradeOption.grade}
+      checked={selectedGrades[criterion.id]?.grade === gradeOption.grade}
+      onChange={() =>
+        handleGradeChange({
+          [criterion.id]: {
+            title: criterion.title,
+            grade: gradeOption.grade,
+            weight: criterion.weight,
+            description: gradeOption.desc,
+          },
+        })
+      }
+      className="num-custom-radio"
+    />
+    <label
+      htmlFor={`${criterion.id}-${gradeOption.grade}`}
+      className="num-custom-radio-label"
+      title={gradeOption.desc}
+    >
+      {gradeOption.grade}
+    </label>
+  </div>
+);
+
+
+const numericalCriterion = ({
+  criterion,
+  selectedGrades,
+  handleGradeChange,
+}: {
+  criterion: NumericalCriteria;
+  selectedGrades: GradeStateContainer;
+  handleGradeChange: (gradeinfo: GradeStateContainer) => void;
+}) => (
+    <div className="criterion-box" key={criterion.id}>
+      <h3>
+        {criterion.title}{" "}
+        <span className="criterion-weight">weight: {criterion.weight} %</span>
+      </h3>
+      <p>{criterion.description}</p>
+
+      {criterion.options.map((gradeOption) => (
+        numericalRadioButtons({
+          criterion,
+          gradeOption,
+          selectedGrades,
+          handleGradeChange,
+        })
+      ))}
+    </div>
+);
 
 export default function NumericalCard({
   oatGrade,
   handleGradeChange,
   selectedGrades,
 }: INumericalCard) {
-  const gradeCardRow = (
+  return (
     <div className="grade-card-row">
       {oatGrade.numerical_criteria &&
         oatGrade.numerical_criteria.map((criterion) => (
-          <div className="criterion-box" key={criterion.id}>
-            <h3>
-              {criterion.title}{" "}
-              <span className="criterion-weight">
-                weight: {criterion.weight} %
-              </span>
-            </h3>
-
-            <p>{criterion.description}</p>
-
-            {criterion.options.map((gradeOption) => (
-              <div className="num-criterion-radio" key={gradeOption.grade}>
-                <input
-                  type="radio"
-                  id={`${criterion.id}-${gradeOption.grade}`}
-                  name={criterion.id}
-                  value={gradeOption.grade}
-                  checked={selectedGrades[criterion.id]?.grade === gradeOption.grade}
-                  onChange={() =>
-                    handleGradeChange({
-                      [criterion.id]: {
-                        title: criterion.title,
-                        grade: gradeOption.grade,
-                        weight: criterion.weight,
-                        description: gradeOption.desc,
-                      },
-                    })
-                  }
-                  className="num-custom-radio"
-                />
-                <label
-                  htmlFor={`${criterion.id}-${gradeOption.grade}`}
-                  className="num-custom-radio-label"
-                  title={gradeOption.desc}
-                >
-                  {gradeOption.grade}
-                </label>
-              </div>
-            ))}
-          </div>
+          numericalCriterion({
+            criterion,
+            selectedGrades,
+            handleGradeChange,
+          })
         ))}
     </div>
   );
-
-  return <>{gradeCardRow}</>;
 }
