d42652d493ea24ee97e9c74a807b1bea6db17426
Jani Sourander <jani.sourander@kamk.fi>
2025-01-22 13:11:32 +0200

    Remove old site

--- arviointi/public/static/js/main.js
+++
@@ -1,9 +0,0 @@
-import { generateProfileSelect } from './helpers/profiles.js';
-
-
-generateProfileSelect();
-
-// Make button invisible
-const gradeButton = document.getElementById('gradeButton');
-gradeButton.style.display = 'none';
-
