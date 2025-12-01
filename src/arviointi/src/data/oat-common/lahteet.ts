aedd762b24fe95e867df629eb7c63ca7ed4a9ae2
Jani Sourander <jani.sourander@kamk.fi>
2025-12-01 09:19:55 +0200

    First draft of 2026 criteria

--- src/arviointi/src/data/oat-common/lahteet.ts
+++ src/arviointi/src/data/oat-common/lahteet.ts
@@ -5,7 +5,10 @@ const common_criterion_lahteet: NumericalCriteria = {
   title: "Lähdeviitteet",
   description: `Hyvin käytetyt lähdeviitteet tukevat kirjoituksen argumentteja. Työ ikään kuin 
   keskustelee lähdeteosten kanssa. Valittujen lähteiden tulee olla luotettavia ja ajantasaisia.
-  Noudata ohjeistettua viittaustyyliä.`,
+  Suosi teoksia, joiden tekijä on selvillä ja mieluiten tunnettu henkilö. Vältä tekoälyn kirjoittamia 
+  tekstejä. Noudata ohjeistettua viittaustyyliä (Vancouver-tyyli Markdown Footnotesia hyödyntäen). 
+  Lähdeluettelon formaatti on KAMK:n kirjaston ohjeistuksen mukainen. Suosi epäsuoria viittauksia (parafrasointia).
+  Kun käytät tekoälyä, tee se läpinäkyvästi. Tekoäly ei ole itsessään luotettava lähde.`,
   weight: -1,
   options: [
     { grade: 0, desc: "Lähdeaineisto on riittämätön." },
@@ -15,23 +18,28 @@ const common_criterion_lahteet: NumericalCriteria = {
     },
     {
       grade: 2,
-      desc: `Lähdeaineistoa ja tekstin sisäiset viittaukset ovat käytössä. Lähteitä on käytetty, 
-      mutta ne eivät ole luotettavia tai eivät tue opiskelijan argumentointia tai niiden yhteys tekstin
-      sisältöön on epäselvä. `,
+      desc: `Lähdeaineistoa ja tekstin sisäiset Footnotes-viittaukset ovat käytössä. 
+      Lähteitä on käytetty, mutta ne eivät ole luotettavia tai eivät tue opiskelijan argumentointia tai niiden yhteys tekstin
+      sisältöön on epäselvä.`,
     },
     {
       grade: 3,
-      desc: `Lähteitä on käytetty, ja ne tukevat opiskelijan argumentointia. Lähdeaineiston 
-      käyttö on systemaattista ja lukijalle selkeää.`,
+      desc: `
+      Lähteitä on käytetty, ja ne tukevat opiskelijan argumentointia.
+      Lähdeaineiston käyttö on systemaattista ja lukijalle selkeää.`,
     },
     {
       grade: 4,
-      desc: `Lähdeaineisto on tarkoituksenmukaista ja monipuolista. Opiskelija on etsinyt itsenäisesti 
-      tietoa, joka auttaa häntä ymmärtämään kurssin aiheita. Teksti käy keskustelua lähteiden kanssa.`,
+      desc: `Lähdeaineisto on tarkoituksenmukaista ja monipuolista. 
+      Opiskelija on etsinyt itsenäisesti tietoa, joka auttaa häntä ymmärtämään kurssin aiheita.
+      Teksti käy keskustelua lähteiden kanssa.
+      Materiaalissa esiin tulevien uusien käsitteiden tai ideoiden tausta ja lähde on perusteltu.`,
     },
     {
       grade: 5,
-      desc: `Lähdeaineisto on kriittisesti arvioitu ja punnittu. Lähteet ovat luotettavia ja huolella valittu.
+      desc: `Lähdeaineisto on kriittisesti arvioitu ja punnittu.
+      Lähteet ovat luotettavia ja huolella valittu.
+      Materiaalissa esiin tulevien uusien käsitteiden tai ideoiden tausta ja lähde on perusteltu.
       Opiskelija esittelee uusia näkökulmia ja argumentoi niitä lähteiden avulla tai lähteiden innoittamana.`,
     },
   ],
--- src/arviointi/src/data/oat-common/tietotaidot.ts
+++ src/arviointi/src/data/oat-common/tietotaidot.ts
@@ -4,34 +4,48 @@ const common_criterion_tietotaidot: NumericalCriteria = {
   id: "self_assessment_criterion1",
   title: "Tietotaidot",
   description: `Korkean tietotaidon omaava opiskelija hallitsee kurssilla käsitellyn käytännön ja teorian todistettavasti.
-  Opiskelija tunnistaa oman osaamisensa. Muista antaa numeraalinen arvosana itsellesi!`,
+  Opiskelija tunnistaa oman osaamisensa. Muista antaa numeraalinen arvosana itsellesi! 
+  Käytä Arviointityökalua apuna: ota kuvakaappaus ja liitä se itsearviointiin. Älä unohda perusteluja!`,
   weight: -1,
   options: [
     { grade: 0, desc: "Ei näyttöä tarvittavista taidoista." },
     {
       grade: 1,
-      desc: `Opiskelija listaa perustaitoja. Kurssin sanasto ja jotkin työvaiheet ovat 
-      läsnä, mutta opiskelijan oma tietotaidon määrä ei ole vahvasti todistettuna.`,
+      desc: `
+      Opiskelija listaa perustaitoja. 
+      Kurssin sanasto ja jotkin työvaiheet ovat läsnä. 
+      Opiskelijan oma tietotaidon määrä ei ole vahvasti todistettuna.`,
     },
     {
       grade: 2,
-      desc: `Opiskelija havainnollistaa perus tietotaitoja. Opiskelija ei tunnista omia taitojaan tai kykene yhdistämään 
-      niitä siihen, miten ne liittyvät kurssin oppimistavoitteisiin.`,
+      desc: `
+      Opiskelija havainnollistaa perus tietotaitoja.
+      Kurssin sanasto ja useimmat työvaiheet ovat läsnä.
+      Opiskelija ei tunnista omia taitojaan tai kykene yhdistämään niitä kurssin oppimistavoitteisiin.`,
     },
     {
       grade: 3,
-      desc: `Opiskelija käyttää kurssilla vaadittuja taitoja ja yhdistää ne teoriaan siten,
-       että aihepiirin ymmärrys ei jää epäselväksi. Opiskelija tunnistaa oman osaamisensa tason.`,
+      desc: `
+      Opiskelija käyttää kurssilla vaadittuja taitoja ja yhdistää ne teoriaan.
+      Aihepiirin ymmärrys ei jää epäselväksi.
+      Opiskelija tunnistaa oman osaamisensa tason numeraalisesti.`,
     },
     {
       grade: 4,
-      desc: `Opiskelija käyttää kurssilla vaadittuja taitoja ja yhdistää ne teoriaan ja itse löytämiinsä 
-      ohjeisiin siten, että tietotaito on selkeästi todistettuna. Opiskelija tunnistaa oman osaamisensa tason.`,
+      desc: `
+      Opiskelija käyttää kurssilla vaadittuja taitoja ja yhdistää ne teoriaan. 
+      Opiskelija on omatoimisesti laajentanut tietotaitoaan viitoitettuun suuntaan.
+      Tietotaito on selkeästi todistettuna ja omaäänisesti esitetty.
+      Opiskelija tunnistaa oman osaamisensa tason numeraalisesti ja sanallisesti.`,
     },
     {
       grade: 5,
-      desc: `Opiskelija soveltaa taitoja, vertailee toimintatapoja ja luo uusia johtopäätöksiä. On täysin selvää, että 
-      opiskelija osaa kurssilla vaaditut asiat. Hän tunnistaa osaamisensa ja mahdolliset kehittämiskohteet itse.`,
+      desc: `
+      Opiskelija soveltaa taitoja, vertailee toimintatapoja ja luo uusia johtopäätöksiä.
+      Opiskelija on laajentanut tietotaitoaan omatoimisesti ja syvällisesti.
+      On täysin selvää, että opiskelija osaa kurssilla vaaditut asiat. 
+      Opiskelija tunnistaa osaamisensa numeraalisesti ja sanallisesti.
+      Opiskelija tunnistaa omat kehityskohteensa.`,
     },
   ],
 };
