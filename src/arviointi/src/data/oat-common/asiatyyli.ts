dba237944e692839b13b7f408f4c29bbf0bb0db6
Jani Sourander <jani.sourander@kamk.fi>
2025-12-01 09:19:55 +0200

    First draft of 2026 criteria

--- src/arviointi/src/data/oat-common/asiatyyli.ts
+++ src/arviointi/src/data/oat-common/asiatyyli.ts
@@ -5,32 +5,46 @@ const common_criterion_asiatyyli: NumericalCriteria = {
   title: "Asiatyyli",
   description: `Asiatyyliin kirjoitettu dokumentti muistuttaa opinnäytetyötä, ammattilaisille 
   kirjoitettua artikkelia (esim. Medium.com) tai raporttia. Palautusformaattina on Material for MkDocs -sivusto ellei kurssin
-  tehtävänannossa erikseen toisin sanota. Noudata Oppimispäiväkirja 101 ohjeita.`,
+  tehtävänannossa erikseen toisin sanota. Noudata Oppimispäiväkirja 101 ohjeita. Viikkonumerot ja päivämäärät vastaavat 
+  totisesti työskentelyaikoja.`,
   weight: -1,
   options: [
     { grade: 0, desc: "Teksti ei ole ymmärrettävää ja/tai asiatyylistä." },
     {
       grade: 1,
-      desc: `Teksti on tyylillisesti jäsentymätöntä. Esiintyy suuria määriä tyyli- ja/tai 
-        kielivirheitä, jotka vaikeuttavat tekstin lukemista, tai teknisiä virheitä, jotka estävät 
-        dokumentaation lukemisen.`,
+      desc: `
+      Teksti on tyylillisesti jäsentymätöntä.
+      Esiintyy suuria määriä tyyli- ja/tai kielivirheitä, jotka vaikeuttavat tekstin lukemista.
+      Vaihtoehtoisesti on teknisiä virheitä, jotka estävät dokumentaation lukemisen ilman opettajan korjausta.`,
     },
-    { grade: 2, desc: `Teksti on tyylillisesti toteavaa, epätasaista, lokikirjoitus 
-      tai lista tai ei muutoin edusta ohjeituksen mukaista tyyliä ja laatua.` },
+    {
+      grade: 2,
+      desc: `
+      Teksti on tyylillisesti toteavaa, epätasaista tai lokikirjoitus.
+      Ei edusta ohjeituksen Oppimispäiväkirja 101:n mukaista tyyliä ja laatua.`,
+    },
     {
       grade: 3,
-      desc: `Teksti on huoliteltua ja tehtävänannon mukaista asiatekstiä. Markdown-kieltä ja
-      asiatyyliä on käytetty Oppimispäiväkirja 101 -ohjeistuksen mukaisesti.`,
+      desc: `
+      Teksti on huoliteltua ja tehtävänannon mukaista asiatekstiä.
+      Teksti referoi kurssimateriaalia ja lähteitä, mutta argumentaatio ja oma ääni puuttuvat.
+      Noudattaa Oppimispäiväkirja 101 -ohjeistusta kauttaaltaan.
+      Viikko ja päivämäärämerkinnät ovat oikein.`,
     },
     {
       grade: 4,
-      desc: `Teksti on sujuvaa ja lähes virheetöntä asiatyyliä, jossa ammattisanasto on käytössä. 
-      Puutteita kuitenkin esiintyy eikä teksti ole viimeisteltyä sisällön tai muodon puolesta.`,
+      desc: `
+      Teksti on sujuvaa ja lähes virheetöntä asiatyyliä, jossa ammattisanasto on käytössä. 
+      Argumentointi ja oivallus ovat ylätasolla, mutta oma ääni on läsnä vain osittain.
+      Tekstiä on rikastettu kuvilla, koodiesimerkeillä, luetteloilla, väliotsikoilla ja muilla elementeillä.`,
     },
     {
       grade: 5,
-      desc: `Teksti on argumentoivaa, oivaltavaa ja lähes virheetöntä asiatyyliä. Tekstin, kuvien, 
-      koodin, luetteloiden, väliotsikoiden ja muiden elementtien käyttö on harkittua ja tasapainoista.`,
+      desc: `
+      Teksti on argumentoivaa, oivaltavaa ja lähes virheetöntä asiatyyliä.
+      Oma ääni on vahvasti läsnä tekstissä.
+      Merkinnät olisivat valmiita julkaistaviksi sopivaan ammatilliseen kontekstiin.
+      Tekstin, kuvien, koodin, luetteloiden, väliotsikoiden ja muiden elementtien käyttö on harkittua ja tasapainoista.`,
     },
   ],
 };
--- src/arviointi/src/data/oat-unique/oppimispaivakirja_gitlog.ts
+++ src/arviointi/src/data/oat-unique/oppimispaivakirja_gitlog.ts
@@ -4,12 +4,16 @@ const unique_oppimispaivakirja_gitlog: NumericalCriteria = {
   id: "oppimispaivakirja_gitlog",
   title: "Opiskeluaktiivisuus",
   description: `Aktiivinen opiskelu edistää oppimista ja samalla harjaannuttaa git-versionhallinnan 
-  käytössä. Sinulta toivotaan viikoittaista työskentelyä, jonka tulee näkyä git-lokeissa.`,
+  käytössä. Sinulta toivotaan viikoittaista työskentelyä, jonka tulee näkyä git-lokeissa. Käytä selkeitä commit-viestejä. 
+  Todista aktiivisuus myös itsearvioinnissa! Apuna voit käyttää GitLab:n tarjomia työkaluja kuten Code > Repository Graph.
+  Aktiivisuus Teamsissä tai Discordissa ei korvaa git-lokeja, mutta lasketaan eduksi. Jos olet ollut aktiivinen, 
+  mainitse se itsearvioinnissa.`,
   weight: 20,
   options: [
     {
       grade: 0,
-      desc: "Työskentelyhistoriasta ei ole näyttöä. Git-historiassa on vain yksi commit juuri ennen palautusta.",
+      desc: `
+      Työskentelyhistoriasta ei ole näyttöä. Git-historiassa on vain yksi commit juuri ennen palautusta.`,
     },
     {
       grade: 1,
@@ -18,24 +22,31 @@ const unique_oppimispaivakirja_gitlog: NumericalCriteria = {
     },
     {
       grade: 2,
-      desc: `Työ on tehty noin kahdessa viikossa.`,
+      desc: `Työ on tehty noin kahdessa viikossa, kuten kurssin aloitusviikolla ja lopetusviikolla.`,
     },
     {
       grade: 3,
-      desc: `Työskentely on ollut aktiivista läpi kurssin, mutta kausittaista. Työtä on tehty noin joka 
-      toinen viikko.`,
+      desc: `
+      Työskentely on ollut aktiivista läpi kurssin, mutta kausittaista.
+      Työtä on tehty noin joka toinen viikko.
+      Työ on tehty pääasiassa kurssin aikataulun mukaisesti.
+      Aikataulusta poikkeamat on sovittu opettajan kanssa ja dokumentoitu selkeästi.`,
     },
     {
       grade: 4,
-      desc: `Opiskelija on tehnyt sisällön kannalta merkittäviä committeja lähes joka viikko. Commitit liittyvät 
-      kyseisen viikon työksentelyyn. Jos viikkoja on hypitty yli, tähän löytyy selkeät syyt dokumentoituna, ja niistä 
-      on keskustelu opettajan kanssa.`,
+      desc: `
+      Opiskelija on tehnyt sisällön kannalta merkittäviä committeja lähes joka viikko.
+      Commit-viesteistä on tulkittavissa, mitä kyseisellä viikolla on tehty.
+      Aikataulusta poikkeamat on sovittu opettajan kanssa ja dokumentoitu selkeästi.
+      Työ on palautettu saman lukukauden aikana kun kurssi on alkanut.`,
     },
     {
       grade: 5,
-      desc: `Opiskelija on ollut erittäin aktiivinen ja git-lokit ovat selkeät ja täydelliset. Kunkin viikon merkinnän 
-      suurin työ on commitoitu kyseisellä viikolla. Mahdolliset poikkeamat on dokumentoitu ja niistä on keskusteltu 
-      opettajan kanssa.`,
+      desc: `
+      Opiskelija on ollut erittäin aktiivinen ja git-lokit ovat selkeät ja täydelliset.
+      Kunkin viikon merkinnän suurin työ on commitoitu kyseisellä viikolla. 
+      Aikataulusta poikkeamat on sovittu opettajan kanssa ja dokumentoitu selkeästi.
+      Työ on palautettu määräaikaan mennessä ellei muuta ole sovittu opettajan kanssa.`,
     },
   ],
 };
